846113b3decd66358e593a63338aa983bd2fc605
Chae Mo <you@ddd.com>
2019-05-01 17:26:24 +0900

    자습(코딩 도장 C기본)05/01/19

---
+++ C_Study/1_CLI/0_Self/getchar_putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	char c1 = getchar();
+	char c2 = 'a';
+
+	printf("%c\n", c1);
+	putchar(c2);
+
+	return 0;
+}
---
+++ C_Study/1_CLI/0_Self/put_int_float_char.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main(){
+    
+    int num1;
+    float num2;
+    char c1;
+    
+    scanf("%d %f %c",&num1, &num2, &c1);
+    
+    printf("%d\n",num1);
+    printf("%f\n",num2);
+    printf("%c",c1);
+}
